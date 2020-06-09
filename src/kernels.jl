937bb67aeda03ee99447c37ff76f3053dd58c271
Basti Kusch <bastikusch@live.com>
2020-06-09 12:35:49 +0200

    kernel change

--- src/kernels.jl
+++ src/kernels.jl
@@ -33,7 +33,7 @@ LaplaceKernel() = LaplaceKernel(1.0)
 ## similarity computation for each kernel
 
 function similarity(k::InverseDistance, x::Vector, y::Vector)
-    return 1 / norm(x .- y)
+    return x == y ? 0.0 : 1 / norm(x .- y)
 end
 
 function similarity(k::Gaussian, x::Vector, y::Vector)
